e8b552fd2411b61e2e52483b4fc56e6788794e1f
GomidesTs <gomidestulio5@gmail.com>
2022-09-25 00:10:51 -0300

    :sparkles: feat: adding routes

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('express-async-errors')
 
 const express = require('express')
 const AppError = require('./utils/AppError')
+const routes = require('./routes')
 const databaseRun = require('./database/sqlite')
 const app = express()
 
@@ -10,6 +11,7 @@ const PORT = 3000
 databaseRun()
 
 app.use(express.json())
+app.use(routes)
 app.use((error, request, response, next) => {
     if (error instanceof AppError) {
         return response.status(error.statusCode).json({

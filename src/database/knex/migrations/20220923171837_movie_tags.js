60418beaf5d41dcbae864e7e9e741ceab9f926ae
GomidesTs <gomidestulio5@gmail.com>
2022-09-28 20:37:50 -0300

    :bug: fix: table name change for connection to be performed correctly

--- src/database/knex/migrations/20220923171837_movie_tags.js
+++ src/database/knex/migrations/20220923171837_movie_tags.js
@@ -1,6 +1,6 @@
 exports.up = knex => knex.schema.createTable('movie_tags', table => {
     table.increments('id').primary().unsigned()
-    table.integer('note_id').references('id').inTable('movies_notes').onDelete('cascade')
+    table.integer('note_id').references('id').inTable('movie_notes').onDelete('cascade')
     table.integer('user_id').references('id').inTable('users')
     table.text('name')
 })
